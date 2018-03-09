b410406acac8831365d4db590875fe101e053632
root <root@anonymous.homenet.telecomitalia.it>
2018-03-09 14:40:04 +0100

    19marzo

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,16 @@
+    
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import static java.lang.System.in;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +22,79 @@
  * @author root
  */
 public class Client {
-    
+
+
+    Socket connection;
+    //nome o IP del server
+    String serverAddress;
+    //porta del server in ascolto
+    int port;
+    private boolean fineCom;
+    String autore;
+
+    void Client() {
+        connection = null;
+        fineCom=false;
+        //nome o IP del server
+    }
+
+    void avvioClient(String serverAddress,int port) {
+        //apertura della connessione al server sulla porta specificata
+        this.serverAddress=serverAddress;
+        this.port=port;
+        System.out.println("tentativo connessione");
+        try {
+            InetAddress address = InetAddress.getByName(serverAddress); //traduzione in formato indirizzo per il socket
+            connection = new Socket(address, port);
+            System.out.println("Connessione aperta");
+        } catch (ConnectException e) {
+            System.err.println("Server non disponibile!");
+        } catch (UnknownHostException e1) {
+            System.err.println("Errore DNS!");
+        } catch (IOException e2) {//
+            System.err.println(e2);
+            e2.printStackTrace();
+        }
+    }
+
+    void invioClient() {
+        try {  
+            System.out.println("inserisci messaggio per il server");
+            InputStreamReader tastiera=new InputStreamReader(System.in);
+            BufferedReader buffer=new BufferedReader(tastiera);
+            String n=buffer.readLine();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            String b = n + "\n";
+            bw.write(b);
+            bw.flush();
+            System.out.println("il messaggio inviato ");
+            if(n.equalsIgnoreCase("end")){
+                fineCom=true;
+            }else{
+                fineCom=false;
+            }           
+
+        } catch (IOException a) {} //chiusura della connnessione
+    }
+    void ricezioneClient() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String a = br.readLine();
+        System.out.println("il messaggio inviato dal server è " + a);
+    }
+    void chiusuraClient(){ 
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connessione chiusa!");
+            }
+        } catch (IOException e) {
+            System.err.println("Errore nella chiusura della connessione!");
+        }    
+    }
+    public boolean stopCom(){
+        return fineCom;
+    }
 }
+
+    
+
--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author root
- */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    
+
+    int port;
+    Socket connection;
+    ServerSocket sSocket;
+    private boolean fineCom,setAut;
+    String autore;
+   
+
+    void Server() {
+        connection = null;
+        sSocket = null;
+        fineCom = false;
+    }
+
+    //oggetto ServerSocket necessario per accettare richieste dal client
+    //oggetto da usare per realizzare la connessione TCP
+    void avvioServer(int port) throws IOException {
+        this.port = port;
+        sSocket = new ServerSocket(port);
+        System.out.println("In attesa di connessioni!");
+        boolean uscita = false;
+        while (uscita == false) {
+
+            connection = sSocket.accept();
+            System.out.println("Socket server: " + connection.getLocalSocketAddress());
+            System.out.println("Socket client: " + connection.getRemoteSocketAddress());
+            if (connection.isConnected() == true) {
+                uscita = true;
+            }
+            System.out.println("Connessione stabilita su porta: " + sSocket.getLocalPort());
+        }
+
+    }
+
+    void invioServer(String n) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        System.out.println("messaggio inviato");
+        String b = n + "\n";
+        bw.write(b);
+        bw.flush();
+
+    }
+
+    String ricezioneServer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String a = br.readLine();
+        System.out.println("il messaggio inviato dal client è " + a);
+        return a;
+       /* if (a.equalsIgnoreCase("end")) {
+            fineCom = true;
+        } else {
+            fineCom = false;
+        }
+        if(a.equalsIgnoreCase("autore")){
+            setAut=true;
+        } else{
+            setAut=false;
+        }*/
+    }
+
+    public void chiusuraServer() throws IOException {
+
+        //chiusura della connessione con il client
+        try {
+            connection.close();
+            sSocket.close();
+        } catch (IOException ex) {
+            System.err.println("Errore nella chiusura della connessione!");
+        }
+        System.out.println("Connessione chiusa!");
+    }
+    public void setAutore(String autore){
+        this.autore=autore;
+    }
 }
